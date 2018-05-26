1bef4e2b0beedf49e20c2c7490814d53e3e1801b
David Doan <s3686810@student.rmit.edu.au>
2018-05-26 16:33:45 +1000

    Reverted change that caused buug

--- a3/accommodation.js
+++ a3/accommodation.js
@@ -1,4 +1,4 @@
-function displaySmallUnpoweredSite() {
+function sus() {
 	var a = document.getElementById("sus");
 	var b = document.getElementById("sps");
 	var c = document.getElementById("mus");
@@ -16,8 +16,8 @@ function displaySmallUnpoweredSite() {
 		e.style.display="none";
 	} else {
 		
-        f.style.display="block";	
-        a.style.display="none";
+        f.style.display="none";	
+        a.style.display= "block";
 		b.style.display="none";
 		c.style.display="none";
 		d.style.display="none";
@@ -25,7 +25,7 @@ function displaySmallUnpoweredSite() {
 	}
 }
 
-function displaySmallPoweredSite() {
+function sps() {
 	var a = document.getElementById("sus");
 	var b = document.getElementById("sps");
 	var c = document.getElementById("mus");
@@ -42,8 +42,8 @@ function displaySmallPoweredSite() {
 		e.style.display="none";
 	} else {
 		
-        f.style.display="block";	
-        b.style.display="none";
+        f.style.display="none";	
+        b.style.display= "block";
 		a.style.display="none";
 		c.style.display="none";
 		d.style.display="none";
@@ -51,7 +51,7 @@ function displaySmallPoweredSite() {
 	}
 }
 
-function displayMediumUnpoweredSite() {
+function mus() {
 	var a = document.getElementById("sus");
 	var b = document.getElementById("sps");
 	var c = document.getElementById("mus");
@@ -68,8 +68,8 @@ function displayMediumUnpoweredSite() {
 		e.style.display="none";
 	} else {
 	
-        f.style.display="block";	
-        c.style.display="none";
+        f.style.display="none";	
+        c.style.display= "block";
 		b.style.display="none";
 		a.style.display="none";
 		d.style.display="none";
@@ -77,7 +77,7 @@ function displayMediumUnpoweredSite() {
 	}
 }
 
-function displayMediumPoweredSite() {
+function mps() {
 	var a = document.getElementById("sus");
 	var b = document.getElementById("sps");
 	var c = document.getElementById("mus");
@@ -94,8 +94,8 @@ function displayMediumPoweredSite() {
 		e.style.display="none";
 	} else {
 		
-        f.style.display="block";	
-        d.style.display="none";
+        f.style.display="none";	
+        d.style.display= "block";
 		b.style.display="none";
 		c.style.display="none";
 		a.style.display="none";
@@ -103,7 +103,7 @@ function displayMediumPoweredSite() {
 	}
 }
 
-function displayCaravanSite() {
+function cs() {
 	var a = document.getElementById("sus");
 	var b = document.getElementById("sps");
 	var c = document.getElementById("mus");
@@ -118,8 +118,8 @@ function displayCaravanSite() {
 		d.style.display="none";
 		a.style.display="none";
 	} else {
-		e.style.display="none";
-        f.style.display="block";	
+		e.style.display= "block";
+        f.style.display="none";	
 		b.style.display="none";
 		c.style.display="none";
 		d.style.display="none";
