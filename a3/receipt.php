4280f927c3f166127c707bf11601c70f829efdcf
David Doan <s3686810@student.rmit.edu.au>
2018-05-27 16:50:07 +1000

    Removed extra '('

--- a3/receipt.php
+++ a3/receipt.php
@@ -4,10 +4,10 @@
     $customer = array (
             'name' =>$_POST['name'],
             'email' =>$_POST['email'],
-            'phone' =>$_POST['phone']));
+            'phone' =>$_POST['phone']);
 
     array_push($_SESSION['booking'], $customer);
-    
+
 print_r($_SESSION);
 require_once("tools.php");
 top_module("Booking", "booking-image", "", "button-primary", "", "");
