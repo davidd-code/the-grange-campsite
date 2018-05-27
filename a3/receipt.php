7fa1a0a8775cd9158b2e33062ea73682734d15ba
David Doan <s3686810@student.rmit.edu.au>
2018-05-27 17:26:53 +1000

    More work on receipt page

--- a3/receipt.php
+++ a3/receipt.php
@@ -10,5 +10,52 @@
 
 print_r($_SESSION);
 require_once("tools.php");
-top_module("Booking", "booking-image", "", "button-primary", "", "");
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+    <!-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <meta charset="utf-8">
+        <title>The Grange | $pageTitle</title>
+        <meta name="description" content="Receipt">
+        <meta name="author" content="David Doan and Josh Miranda">
+
+    <!-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Supermercado+One" rel="stylesheet">
+
+    <!-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/skeleton.css">
+        <link rel="stylesheet" href="style.css">
+        
+    <!-- The Grange
+    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+        <link rel="icon" type="image/png" href="../media/blacklogo.png">
+
+    </head>
+
+    <header class=>
+        <a href="index.php">
+            <img class="logo" src="../media/companyLogo.png" alt="Simplified fireplace with text saying the Grange under it">
+        </a>
+    </header>
+    <hr>    
+
+    <body class="dark-background">
+        <page class="light-background" size="A4">
+            
+        </page>
+    </body>
+</html>
