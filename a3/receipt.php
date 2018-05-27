33479941da5fe58a914ccc5eba0598a7e9fc7bd9
David Doan <s3686810@student.rmit.edu.au>
2018-05-27 16:41:31 +1000

    Changed to session variable

--- a3/receipt.php
+++ a3/receipt.php
@@ -1,18 +1,11 @@
 <?php
     session_start();
 
-    $_SESSION['booking'] = array (
-        'aid' =>$_POST['aid'],
-        'date' =>$_POST['date'],
-        'days' =>$_POST['days'],
-        'adults' =>$_POST['adults'],
-        'children' =>$_POST['children'],
-        'customer' => array (
+    array_push($_SESSION['booking'], 'customer' => array (
             'name' =>$_POST['name'],
             'email' =>$_POST['email'],
-            'phone' =>$_POST['phone']
-        )
-    );
+            'phone' =>$_POST['phone']));
+    
 print_r($_SESSION);
 require_once("tools.php");
 top_module("Booking", "booking-image", "", "button-primary", "", "");
