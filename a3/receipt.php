0da13e8da1d5bea1dc583077ca1b9628ff78dd05
David Doan <s3686810@student.rmit.edu.au>
2018-05-27 16:42:59 +1000

    Changed '=>' to '='

--- a3/receipt.php
+++ a3/receipt.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
 
-    array_push($_SESSION['booking'], 'customer' => array (
+    array_push($_SESSION['booking'], 'customer' = array (
             'name' =>$_POST['name'],
             'email' =>$_POST['email'],
             'phone' =>$_POST['phone']));
-    
+
 print_r($_SESSION);
 require_once("tools.php");
 top_module("Booking", "booking-image", "", "button-primary", "", "");
